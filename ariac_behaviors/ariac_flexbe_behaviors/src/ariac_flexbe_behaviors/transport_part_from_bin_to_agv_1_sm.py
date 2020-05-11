c0ab1662a0ac65e3e6d686eadaac36d70caa8fc4
wessel koolen <wessel.koolen@gmail.com>
2020-05-11 23:17:13 +0200

    alstu de robot beweegt

--- ariac_behaviors/ariac_flexbe_behaviors/src/ariac_flexbe_behaviors/transport_part_from_bin_to_agv_1_sm.py
+++ ariac_behaviors/ariac_flexbe_behaviors/src/ariac_flexbe_behaviors/transport_part_from_bin_to_agv_1_sm.py
@@ -11,6 +11,7 @@ from flexbe_core import Behavior, Autonomy, OperatableStateMachine, ConcurrencyC
 from ariac_flexbe_states.start_assignment_state import StartAssignment
 from ariac_flexbe_states.end_assignment_state import EndAssignment
 from ariac_flexbe_behaviors.transport_part_form_bin_to_agv_state_sm import transport_part_form_bin_to_agv_stateSM
+from ariac_flexbe_states.srdf_state_to_moveit_ariac_state import SrdfStateToMoveitAriac
 # Additional imports can be added inside the following tags
 # [MANUAL_IMPORT]
 
@@ -46,11 +47,20 @@ class transport_part_from_bin_to_agv_1SM(Behavior):
 
 
 	def create(self):
-		# x:649 y:50, x:296 y:171
+		# x:1198 y:186, x:517 y:426
 		_state_machine = OperatableStateMachine(outcomes=['finished', 'failed'])
 		_state_machine.userdata.agv_id = 'agv1'
 		_state_machine.userdata.part_type = 'gear_part'
 		_state_machine.userdata.pose_on_agv = []
+		_state_machine.userdata.config_name_home = 'home'
+		_state_machine.userdata.move_group = 'manipulator'
+		_state_machine.userdata.move_group_prefix = '/ariac/arm1'
+		_state_machine.userdata.action_topic = '/move_group'
+		_state_machine.userdata.robot_name = ''
+		_state_machine.userdata.joint_values = []
+		_state_machine.userdata.joint_names = []
+		_state_machine.userdata.config_name_bin5PreGrasp = 'bin5PreGrasp'
+		_state_machine.userdata.config_name_tray2PreDrop = 'tray2PreDrop'
 
 		# Additional creation code can be added inside the following tags
 		# [MANUAL_CREATE]
@@ -62,21 +72,42 @@ class transport_part_from_bin_to_agv_1SM(Behavior):
 			# x:52 y:43
 			OperatableStateMachine.add('StartAssignment',
 										StartAssignment(),
-										transitions={'continue': 'transport_part_form_bin_to_agv_state'},
+										transitions={'continue': 'MoveR1Home'},
 										autonomy={'continue': Autonomy.Off})
 
-			# x:445 y:45
+			# x:1081 y:69
 			OperatableStateMachine.add('EndAssignment',
 										EndAssignment(),
 										transitions={'continue': 'finished'},
 										autonomy={'continue': Autonomy.Off})
 
-			# x:188 y:44
+			# x:792 y:45
 			OperatableStateMachine.add('transport_part_form_bin_to_agv_state',
 										self.use_behavior(transport_part_form_bin_to_agv_stateSM, 'transport_part_form_bin_to_agv_state'),
 										transitions={'finished': 'EndAssignment', 'failed': 'failed'},
 										autonomy={'finished': Autonomy.Inherit, 'failed': Autonomy.Inherit},
 										remapping={'part_type': 'part_type', 'agv_id': 'agv_id', 'pose_on_agv': 'pose_on_agv'})
+
+			# x:205 y:45
+			OperatableStateMachine.add('MoveR1Home',
+										SrdfStateToMoveitAriac(),
+										transitions={'reached': 'MoveBin5PreGrasp', 'planning_failed': 'failed', 'control_failed': 'failed', 'param_error': 'failed'},
+										autonomy={'reached': Autonomy.Off, 'planning_failed': Autonomy.Off, 'control_failed': Autonomy.Off, 'param_error': Autonomy.Off},
+										remapping={'config_name': 'config_name_home', 'move_group': 'move_group', 'move_group_prefix': 'move_group_prefix', 'action_topic': 'action_topic', 'robot_name': 'robot_name', 'config_name_out': 'config_name_out', 'move_group_out': 'move_group_out', 'robot_name_out': 'robot_name_out', 'action_topic_out': 'action_topic_out', 'joint_values': 'joint_values', 'joint_names': 'joint_names'})
+
+			# x:397 y:33
+			OperatableStateMachine.add('MoveBin5PreGrasp',
+										SrdfStateToMoveitAriac(),
+										transitions={'reached': 'MoveTray2PreDrop', 'planning_failed': 'failed', 'control_failed': 'failed', 'param_error': 'failed'},
+										autonomy={'reached': Autonomy.Off, 'planning_failed': Autonomy.Off, 'control_failed': Autonomy.Off, 'param_error': Autonomy.Off},
+										remapping={'config_name': 'config_name_bin5PreGrasp', 'move_group': 'move_group', 'move_group_prefix': 'move_group_prefix', 'action_topic': 'action_topic', 'robot_name': 'robot_name', 'config_name_out': 'config_name_out', 'move_group_out': 'move_group_out', 'robot_name_out': 'robot_name_out', 'action_topic_out': 'action_topic_out', 'joint_values': 'joint_values', 'joint_names': 'joint_names'})
+
+			# x:588 y:35
+			OperatableStateMachine.add('MoveTray2PreDrop',
+										SrdfStateToMoveitAriac(),
+										transitions={'reached': 'transport_part_form_bin_to_agv_state', 'planning_failed': 'failed', 'control_failed': 'failed', 'param_error': 'failed'},
+										autonomy={'reached': Autonomy.Off, 'planning_failed': Autonomy.Off, 'control_failed': Autonomy.Off, 'param_error': Autonomy.Off},
+										remapping={'config_name': 'config_name_tray2PreDrop', 'move_group': 'move_group', 'move_group_prefix': 'move_group_prefix', 'action_topic': 'action_topic', 'robot_name': 'robot_name', 'config_name_out': 'config_name_out', 'move_group_out': 'move_group_out', 'robot_name_out': 'robot_name_out', 'action_topic_out': 'action_topic_out', 'joint_values': 'joint_values', 'joint_names': 'joint_names'})
 
 
 		return _state_machine
